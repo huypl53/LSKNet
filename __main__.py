008aeb4a7e043adbcc0989398ed210d7d8eb82dd
huypl53 <phamlehuy52@gmail.com>
2024-09-26 13:51:15 +0700

    feat: demo huge image

--- __main__.py
+++ __main__.py
@@ -17,66 +17,63 @@ from log import logger
 
 
 async def async_main():
-    a_session = anext(get_db())
-    session = await (a_session)
-    stmt = (
-        select(TaskMd)
-        .where(TaskMd.task_type == 5)
-        .where(TaskMd.task_stat < 0)
-        .order_by(TaskMd.task_stat.desc())
-    )
-    results = await session.execute(stmt)
-    mapping_results = results.mappings().all()
-    tasks: List[TaskMd] = [m["TaskMd"] for m in mapping_results]
+    while True:
+        a_session = anext(get_db())
+        session = await (a_session)
+        stmt = (
+            select(TaskMd)
+            .where(TaskMd.task_type == 5)
+            .where(TaskMd.task_stat < 0)
+            .order_by(TaskMd.task_stat.desc())
+        )
+        results = await session.execute(stmt)
+        mapping_results = results.mappings().all()
+        tasks: List[TaskMd] = [m["TaskMd"] for m in mapping_results]
 
-    params: DetectShipParam = None
-    model = None
-    print("----------")
-    try:
-        for i, t in enumerate(tasks):
-            if i == 1:
-                break  # update only one
-            param_dict = json.loads(t.task_param)
-            if not t.task_param:
-                params = DetectShipParam()
-            elif ((not params) or t.task_param != params.model_dump()):
-                try:
-                    params = DetectShipParam(**param_dict)
-                except Exception as e:
-                    t.task_stat = 0
-                    t.task_message = str(e)
-            else:
-                t.task_stat = 0  # task got error
-                t.task_message = 'Init model failed!'
-                continue
-            model = init_detector(
-                params.config, params.checkpoint, device=params.device)
+        params: DetectShipParam = None
+        model = None
+        print("----------")
+        try:
+            for i, t in enumerate(tasks):
+                if i == 1:
+                    break  # update only one
+                param_dict = json.loads(t.task_param)
+                if not t.task_param:
+                    params = DetectShipParam()
+                elif ((not params) or t.task_param != params.model_dump()):
+                    try:
+                        params = DetectShipParam(**param_dict)
+                    except Exception as e:
+                        t.task_stat = 0
+                        t.task_message = str(e)
+                else:
+                    t.task_stat = 0  # task got error
+                    t.task_message = 'Init model failed!'
+                    continue
+                model = init_detector(
+                    params.config, params.checkpoint, device=params.device)
 
-            # if not model:
-            #     t.task_stat = 0  # task got error
-            #     t.task_message = 'Init model failed!'
-            #     continue
-            im = read_ftp_image(params.input_file)
-            result = inference_detector_by_patches(model, im, params.patch_sizes,
-                                                   params.patch_steps, params.img_ratios,
-                                                   params.merge_iou_thr)  # inference for batch
-            if not len(result):
+                im = read_ftp_image(params.input_file)
+                result = inference_detector_by_patches(model, im, params.patch_sizes,
+                                                       params.patch_steps, params.img_ratios,
+                                                       params.merge_iou_thr)  # inference for batch
+                if not len(result):
+                    t.task_stat = 1
+                    t.task_message = 'No detection'
+                    continue
+                output = result[0]
+                t.task_output = json.dumps(output)
                 t.task_stat = 1
-                t.task_message = 'No detection'
-                continue
-            output = result[0]
-            t.task_output = json.dumps(output)
-            t.task_stat = 1
-            t.task_message = 'Successful'
-            t.task_param = json.dumps(params.model_dump())
-            t.process_id = os.getpid()
-    except Exception as e:
-        logger.error(e)
+                t.task_message = 'Successful'
+                t.task_param = json.dumps(params.model_dump())
+                t.process_id = os.getpid()
+        except Exception as e:
+            logger.error(e)
 
-    print("----------")
-    await session.commit()
-    # await asyncio.sleep(3)
-    await session.close()
+        print("----------")
+        await session.commit()
+        await session.close()
+        await asyncio.sleep(3)
 
 
 # Run this from outter directory
--- demo/huge_image_demo.py
+++ demo/huge_image_demo.py
@@ -12,7 +12,6 @@ python demo/huge_image_demo.py \
 """  # nowq
 
 from argparse import ArgumentParser
-
 from mmdet.apis import init_detector, show_result_pyplot
 
 from mmrotate.apis import inference_detector_by_patches
@@ -20,41 +19,46 @@ from mmrotate.apis import inference_detector_by_patches
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('img', help='Image file')
-    parser.add_argument('config', help='Config file')
-    parser.add_argument('checkpoint', help='Checkpoint file')
+    parser.add_argument("imgs", help="Image files", nargs="+", default=[])
+    parser.add_argument("config", help="Config file")
+    parser.add_argument("checkpoint", help="Checkpoint file")
     parser.add_argument(
-        '--patch_sizes',
+        "--patch_sizes",
         type=int,
-        nargs='+',
+        nargs="+",
         default=[1024],
-        help='The sizes of patches')
+        help="The sizes of patches",
+    )
     parser.add_argument(
-        '--patch_steps',
+        "--patch_steps",
         type=int,
-        nargs='+',
+        nargs="+",
         default=[824],
-        help='The steps between two patches')
+        help="The steps between two patches",
+    )
     parser.add_argument(
-        '--img_ratios',
+        "--img_ratios",
         type=float,
-        nargs='+',
+        nargs="+",
         default=[1.0],
-        help='Image resizing ratios for multi-scale detecting')
+        help="Image resizing ratios for multi-scale detecting",
+    )
     parser.add_argument(
-        '--merge_iou_thr',
+        "--merge_iou_thr",
         type=float,
         default=0.1,
-        help='IoU threshould for merging results')
+        help="IoU threshould for merging results",
+    )
+    parser.add_argument("--device", default="cuda:0", help="Device used for inference")
     parser.add_argument(
-        '--device', default='cuda:0', help='Device used for inference')
+        "--palette",
+        default="dota",
+        choices=["dota", "sar", "hrsc", "hrsc_classwise", "random"],
+        help="Color palette used for visualization",
+    )
     parser.add_argument(
-        '--palette',
-        default='dota',
-        choices=['dota', 'sar', 'hrsc', 'hrsc_classwise', 'random'],
-        help='Color palette used for visualization')
-    parser.add_argument(
-        '--score-thr', type=float, default=0.3, help='bbox score threshold')
+        "--score-thr", type=float, default=0.3, help="bbox score threshold"
+    )
     args = parser.parse_args()
     return args
 
@@ -63,18 +67,27 @@ def main(args):
     # build the model from a config file and a checkpoint file
     model = init_detector(args.config, args.checkpoint, device=args.device)
     # test a huge image by patches
-    result = inference_detector_by_patches(model, args.img, args.patch_sizes,
-                                           args.patch_steps, args.img_ratios,
-                                           args.merge_iou_thr)
-    # show the results
-    show_result_pyplot(
-        model,
-        args.img,
-        result,
-        palette=args.palette,
-        score_thr=args.score_thr)
+    for im_path in args.imgs:
+        print(f"Processing {im_path}")
+        result = inference_detector_by_patches(
+            model,
+            im_path,
+            args.patch_sizes,
+            args.patch_steps,
+            args.img_ratios,
+            args.merge_iou_thr,
+        )
+        # show the results
+        show_result_pyplot(
+            model,
+            im_path,
+            result,
+            palette=args.palette,
+            score_thr=args.score_thr,
+            out_file="_result".join([im_path.rsplit(".", 1)[0], ".jpg"]),
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
     main(args)
